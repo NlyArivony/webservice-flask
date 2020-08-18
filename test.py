afd2123d839f3209635b6a845274fbaddc0cfc85
nely <rambnel@gmail.com>
2020-08-18 16:28:36 +0200

    wxcwc

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def hello():
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello ssdfsdf {}!".format(name)
+    return "Hello ssdfsdf sdfsdfdsfsdfdsfs ssdfdsfcxvxcvvcxvcvx{}!".format(name)
 
 if __name__ == '__main__':
     app.run()
