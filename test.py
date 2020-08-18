e22352206a02271b101bd6f23cd8c09b1fe6db35
nely <rambnel@gmail.com>
2020-08-18 17:37:31 +0200

    Auto stash before revert of "sparta"

--- test.py
+++ test.py
@@ -7,7 +7,11 @@ def hello():
 
 @app.route('/<name>')
 def hello_name(name):
+<<<<<<< HEAD
     return "master voalohany!".format(name)
+=======
+    return "karak{}!".format(name)
+>>>>>>> parent of e74976c... sparta
 
 if __name__ == '__main__':
     app.run()
