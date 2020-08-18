d771661c2c51ed90fc7815b4b854cf65d049b23e
nely <rambnel@gmail.com>
2020-08-18 16:26:52 +0200

    qsdqd

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def hello():
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format(name)
+    return "Hello ssdfsdf {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
